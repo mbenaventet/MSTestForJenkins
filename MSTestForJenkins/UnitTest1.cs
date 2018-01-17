c50dae0b613f5b1e7fff90f322e6dba5c3f7ed33
Manuel Benavente <mbenaventet@gmail.com>
2018-01-17 15:37:28 -0500

    Added a failed test

--- MSTestForJenkins/UnitTest1.cs
+++ MSTestForJenkins/UnitTest1.cs
@@ -7,9 +7,14 @@ namespace MSTestForJenkins
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_Passed()
         {
             Assert.AreEqual(true, true);
+        }
+        [TestMethod]
+        public void TestMethod_Fail()
+        {
+            Assert.AreEqual(true, false);
         }
     }
 }
